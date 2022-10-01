5a5e57118c8289fcf97febb01e15df33ac46292c
Prince <princev2451@gmail.com>
2022-10-02 01:13:27 +0530

    ✨ forgot password page added

---
+++ src/pages/auth/forgot-password.tsx
@@ -0,0 +1,69 @@
+import { NextPage } from "next";
+import React from "react";
+import Wrapper from "../../elements/auth/wrapper";
+import {
+  Paper,
+  Text,
+  TextInput,
+  Button,
+  Group,
+  Center,
+  Box,
+  createStyles,
+} from "@mantine/core";
+import { IconArrowLeft } from "@tabler/icons";
+import SecurityIcon from "../../../public/assets/icons/security.svg";
+import Image from "next/image";
+import Link from "../../components/navigation/link";
+
+const useStyles = createStyles((theme) => ({
+  controls: {
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column-reverse",
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan("xs")]: {
+      width: "100%",
+      textAlign: "center",
+    },
+  },
+}));
+
+const ForgotPassword: NextPage = () => {
+  const { classes } = useStyles();
+
+  return (
+    <Wrapper
+      title="Forgot password?"
+      titleIcon={
+        <Image width={40} height={40} src={SecurityIcon} alt="security" />
+      }
+    >
+      <Text color="dimmed" size="sm" align="center">
+        Enter your email to get a reset link
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+        <TextInput label="Your email" placeholder="you@email.com" required />
+        <Group position="apart" mt="lg" className={classes.controls}>
+          <Link
+            href="login"
+            color="dimmed"
+            size="sm"
+            className={classes.control}
+          >
+            <Center inline>
+              <IconArrowLeft size={12} stroke={1.5} />
+              <Box ml={5}>Back to login page</Box>
+            </Center>
+          </Link>
+          <Button className={classes.control}>Reset password</Button>
+        </Group>
+      </Paper>
+    </Wrapper>
+  );
+};
+
+export default ForgotPassword;
