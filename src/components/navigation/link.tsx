cc3390a04ffd926587dbfa89c7ec218826286909
Prince <princev2451@gmail.com>
2022-10-02 16:31:03 +0530

    👔 link active classname added

--- src/components/navigation/link.tsx
+++ src/components/navigation/link.tsx
@@ -1,16 +1,34 @@
 import React from "react";
 import NextLink from "next/link";
-import { Anchor, AnchorProps } from "@mantine/core";
+import { Anchor, AnchorProps, clsx } from "@mantine/core";
 import type { PolymorphicComponentProps } from "@mantine/utils";
+import { useRouter } from "next/router";
 
 interface LinkProps extends PolymorphicComponentProps<"a", AnchorProps> {
   href: string;
+  activeClassName?: string;
+  isExact?: boolean;
 }
 
-const Link: React.FC<LinkProps> = ({ href, ...props }) => {
+const Link: React.FC<LinkProps> = ({
+  href,
+  className,
+  activeClassName = "",
+  isExact = false,
+  ...props
+}) => {
+  const router = useRouter();
+
   return (
     <NextLink href={href} passHref>
-      <Anchor {...props}></Anchor>
+      <Anchor
+        className={clsx(className, {
+          [activeClassName]: isExact
+            ? router.pathname === href
+            : router.pathname.startsWith(href),
+        })}
+        {...props}
+      ></Anchor>
     </NextLink>
   );
 };
