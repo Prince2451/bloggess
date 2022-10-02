df033c12f8beefec4282bb7e4cc81787039a2e4b
Prince <princev2451@gmail.com>
2022-10-02 18:23:34 +0530

    💄 app shell styles updated

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -7,6 +7,21 @@ const mantineTheme: MantineThemeOverride = {
     fontFamily:
       "Poppins, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji",
   },
+  cursorType: "pointer",
+  components: {
+    AppShell: {
+      styles(theme) {
+        return {
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[1],
+          },
+        };
+      },
+    },
+  },
 };
 
 export { mantineTheme };
