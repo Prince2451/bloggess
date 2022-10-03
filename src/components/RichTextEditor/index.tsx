2104f9fc2cb511c4c1b1f2efa1dc4e43fe4831a2
Prince <princev2451@gmail.com>
2022-10-03 23:32:57 +0530

    👔 Added wrapper file for rte

---
+++ src/components/RichTextEditor/index.tsx
@@ -0,0 +1,11 @@
+import dynamic from "next/dynamic";
+
+const RichTextEditor = dynamic(() => import("@mantine/rte"), {
+  // Disable during server side rendering
+  ssr: false,
+
+  // Render anything as fallback on server, e.g. loader or html content without editor
+  loading: () => null,
+});
+
+export default RichTextEditor;
