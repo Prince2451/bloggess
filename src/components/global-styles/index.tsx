4e860f33176c33ddd78144d372c13e2cb8e4f652
Prince <princev2451@gmail.com>
2022-10-04 14:14:06 +0530

    💄 code highlighting added

---
+++ src/components/global-styles/index.tsx
@@ -0,0 +1,8 @@
+import { Global } from "@mantine/core";
+import React from "react";
+
+const GlobalStyles: React.FC = () => {
+  return <Global styles={() => ({})} />;
+};
+
+export default GlobalStyles;
