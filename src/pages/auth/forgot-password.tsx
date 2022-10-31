8be648763cf299cb1013223cf101f63aa47cabd5
Prince <princev2451@gmail.com>
2022-10-31 23:44:43 +0530

    ✨ validation in forgot password added

--- src/pages/auth/forgot-password.tsx
+++ src/pages/auth/forgot-password.tsx
@@ -15,6 +15,8 @@ import { IconArrowLeft } from "@tabler/icons";
 import SecurityIcon from "../../../public/assets/icons/security.svg";
 import Image from "next/legacy/image";
 import Link from "../../components/navigation/link";
+import { useForm, zodResolver } from "@mantine/form";
+import { z } from "zod";
 
 const useStyles = createStyles((theme) => ({
   controls: {
@@ -31,7 +33,18 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const schema = z.object({
+  email: z.string().email({ message: "Valid email address is required" }),
+});
+
 const ForgotPassword: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+    },
+    validate: zodResolver(schema),
+    validateInputOnBlur: true,
+  });
   const { classes } = useStyles();
 
   return (
@@ -44,24 +57,32 @@ const ForgotPassword: NextPage = () => {
       <Text color="dimmed" size="sm" align="center">
         Enter your email to get a reset link
       </Text>
-
-      <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
-        <TextInput label="Your email" placeholder="you@email.com" required />
-        <Group position="apart" mt="lg" className={classes.controls}>
-          <Link
-            href="login"
-            color="dimmed"
-            size="sm"
-            className={classes.control}
-          >
-            <Center inline>
-              <IconArrowLeft size={12} stroke={1.5} />
-              <Box ml={5}>Back to login page</Box>
-            </Center>
-          </Link>
-          <Button className={classes.control}>Reset password</Button>
-        </Group>
-      </Paper>
+      <form onSubmit={form.onSubmit(() => void null)}>
+        <Paper withBorder shadow="md" p={30} radius="md" mt="xl">
+          <TextInput
+            label="Your email"
+            placeholder="you@email.com"
+            withAsterisk
+            {...form.getInputProps("email")}
+          />
+          <Group position="apart" mt="lg" className={classes.controls}>
+            <Link
+              href="login"
+              color="dimmed"
+              size="sm"
+              className={classes.control}
+            >
+              <Center inline>
+                <IconArrowLeft size={12} stroke={1.5} />
+                <Box ml={5}>Back to login page</Box>
+              </Center>
+            </Link>
+            <Button type="submit" className={classes.control}>
+              Reset password
+            </Button>
+          </Group>
+        </Paper>
+      </form>
     </Wrapper>
   );
 };
