ce14edfd2e82e66ac612435ed6a8fc7455c4125b
Prince <princev2451@gmail.com>
2022-10-31 23:39:58 +0530

    ✨ login validation added

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -13,8 +13,24 @@ import Image from "next/legacy/image";
 import Link from "../../components/navigation/link";
 import Wrapper from "../../elements/auth/wrapper";
 import CelebrationsIcon from "../../../public/assets/icons/celebration-user.svg";
+import { z } from "zod";
+import { useForm, zodResolver } from "@mantine/form";
+
+const schema = z.object({
+  email: z.string().email({ message: "Valid email address is required" }),
+  password: z.string().min(1, { message: "Password must not be empty" }),
+});
 
 const Login: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: zodResolver(schema),
+    validateInputOnBlur: true,
+  });
+
   return (
     <Wrapper
       title="Welcome Back!"
@@ -27,30 +43,40 @@ const Login: NextPage = () => {
         />
       }
     >
-      <Text color="dimmed" size="sm" align="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Link href="register" size="sm">
-          Create account
-        </Link>
-      </Text>
+      <form onSubmit={form.onSubmit(() => void null)}>
+        <Text color="dimmed" size="sm" align="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Link href="register" size="sm">
+            Create account
+          </Link>
+        </Text>
 
-      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
-        <Stack>
-          <TextInput label="Email" placeholder="you@email.com" required />
-          <PasswordInput
-            label="Password"
-            placeholder="Your password"
-            required
-          />
-          <Group position="apart">
-            <Checkbox label="Remember me" />
-            <Link href="forgot-password" size="sm">
-              Forgot password?
-            </Link>
-          </Group>
-          <Button fullWidth>Sign in</Button>
-        </Stack>
-      </Paper>
+        <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+          <Stack>
+            <TextInput
+              label="Email"
+              placeholder="you@email.com"
+              withAsterisk
+              {...form.getInputProps("email")}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Your password"
+              withAsterisk
+              {...form.getInputProps("password")}
+            />
+            <Group position="apart">
+              <Checkbox label="Remember me" />
+              <Link href="forgot-password" size="sm">
+                Forgot password?
+              </Link>
+            </Group>
+            <Button type="submit" fullWidth>
+              Sign in
+            </Button>
+          </Stack>
+        </Paper>
+      </form>
     </Wrapper>
   );
 };
