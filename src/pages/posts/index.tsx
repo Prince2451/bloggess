06710c7a26e76a3fc7d0a50dfbbc4cfaec500f91
Prince <princev2451@gmail.com>
2022-11-11 22:17:16 +0530

    💄 table modified

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,20 +1,44 @@
 import { DataTable, DataTableColumn } from "mantine-datatable";
 import React, { useState } from "react";
 import { Post } from "../../types/elements/auth";
-import { Badge, BadgeProps } from "@mantine/core";
+import {
+  Badge,
+  BadgeProps,
+  Box,
+  createStyles,
+  Group,
+  Paper,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import { capitalize } from "lodash";
-import { useRouter } from "next/router";
 import withAuth from "../../hoc/withAuth";
 import { NextPageWithLayout } from "../../types/utils";
+import Image from "next/image";
+import dayjs from "dayjs";
+
+const useStyles = createStyles((theme) => ({
+  postIndex: {
+    ...theme.fn.variant({ variant: "filled" }),
+    width: 20,
+    height: 20,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: theme.spacing.sm,
+    borderRadius: theme.radius.sm,
+  },
+}));
 
 const Posts: NextPageWithLayout = () => {
   const [page, setPage] = useState(1);
-  const router = useRouter();
+  const { classes } = useStyles();
+  const theme = useMantineTheme();
 
   const posts: Array<Post> = [
     {
       category: "nature",
-      coverImage: "",
+      coverImage: "https://source.unsplash.com/random/",
       createdOn: "2022-09-12T12:33.22",
       id: 2,
       slug: "nature-2",
@@ -22,7 +46,7 @@ const Posts: NextPageWithLayout = () => {
     },
     {
       category: "engineering",
-      coverImage: "",
+      coverImage: "https://source.unsplash.com/random/",
       createdOn: "2022-09-12T12:33.22",
       id: 2,
       slug: "nature-2",
@@ -30,7 +54,7 @@ const Posts: NextPageWithLayout = () => {
     },
     {
       category: "nature",
-      coverImage: "",
+      coverImage: "https://source.unsplash.com/random/",
       createdOn: "2022-09-12T12:33.22",
       id: 2,
       slug: "nature-2",
@@ -46,41 +70,61 @@ const Posts: NextPageWithLayout = () => {
   const columns: Array<DataTableColumn<Post>> = [
     {
       title: "#",
-      render: (post) => posts.indexOf(post) + 1,
+      render: (post) => (
+        <Box className={classes.postIndex}>{posts.indexOf(post) + 1}</Box>
+      ),
       accessor: "index",
     },
     {
       title: "Title",
       accessor: "title",
+      render: (post) => (
+        <Group>
+          <Image
+            src={post.coverImage + "?" + post.category}
+            alt="Cover Image"
+            width={40}
+            height={40}
+            style={{ borderRadius: theme.radius.sm }}
+          />
+          <Text>{post.title}</Text>
+        </Group>
+      ),
+    },
+    {
+      title: "Posted On",
+      accessor: "createdOn",
+      render: (post) => (
+        <Text>{dayjs(post.createdOn).format("DD MMM YYYY")}</Text>
+      ),
     },
     {
       title: "Category",
       render: (post) => (
-        <Badge color={badgeColors[post.category] || "gray"}>
+        <Badge
+          size="md"
+          variant="filled"
+          color={badgeColors[post.category] || "gray"}
+        >
           {capitalize(post.category)}
         </Badge>
       ),
       accessor: "category",
     },
-    {
-      title: "Posted On",
-      accessor: "createdOn",
-    },
   ];
 
   return (
-    <DataTable
-      columns={columns}
-      records={posts.concat(posts, posts, posts, posts, posts)}
-      striped
-      highlightOnHover
-      page={page}
-      onPageChange={setPage}
-      recordsPerPage={10}
-      totalRecords={posts.length}
-      minHeight={10}
-      onRowClick={(post) => router.push(router.pathname + "/" + post.slug)}
-    />
+    <Paper p={theme.spacing.md} radius={theme.radius.md}>
+      <DataTable
+        columns={columns}
+        records={posts}
+        page={page}
+        onPageChange={setPage}
+        recordsPerPage={10}
+        totalRecords={posts.length}
+        verticalSpacing={theme.spacing.sm}
+      />
+    </Paper>
   );
 };
 
