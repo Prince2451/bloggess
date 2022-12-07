856343d0d3e1e27c6dfea2955f60c35253054dbb
Prince <princev2451@gmail.com>
2022-12-08 00:06:33 +0530

    👔 useAddPosts added for mutations

---
+++ src/query/posts/useAddPosts.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createPost, uploadCoverImage } from "../../services/posts";
+import { PostFormFields } from "../../types/elements/posts";
+import postsKeys from "./keys";
+
+function useAddPosts() {
+  const queryClient = useQueryClient();
+  const mutation = useMutation(create, {
+    onSuccess: () => queryClient.invalidateQueries(postsKeys.all),
+  });
+
+  async function create(payload: PostFormFields) {
+    const fd = new FormData();
+    if (!payload.coverImage.value) throw new Error("Image not uploaded");
+    fd.append("image", payload.coverImage.value);
+    const { data } = await uploadCoverImage(fd);
+    await createPost({
+      title: payload.title,
+      content: payload.content,
+      coverImage: {
+        base64url: data.base64Url,
+        url: data.url,
+      },
+      description: payload.description,
+      tags: [],
+      categories: ["unknown"],
+    });
+  }
+
+  return mutation;
+}
+
+export default useAddPosts;
