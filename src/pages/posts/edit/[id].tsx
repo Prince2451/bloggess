078686c8ce5840a6c4d2d23bd2d471c369107f5f
Prince <princev2451@gmail.com>
2022-12-13 23:46:24 +0530

    ✨ Post edit support added

--- src/pages/posts/edit/[id].tsx
+++ src/pages/posts/edit/[id].tsx
@@ -1,11 +1,73 @@
+import { Box, LoadingOverlay } from "@mantine/core";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 import Layout from "../../../elements/posts/layout";
 import PostForm from "../../../elements/posts/post-form";
 import withAuth from "../../../hoc/withAuth";
+import { useEditPost, usePost } from "../../../query/posts";
+import { PostFormFields } from "../../../types/elements/posts";
 import { NextPageWithLayout } from "../../../types/utils";
+import { getErrorMessage, showNotification } from "../../../utils";
 
 const PostEdit: NextPageWithLayout = () => {
-  const onSubmit = () => null;
-  return <PostForm onSubmit={onSubmit} isLoading={false} />;
+  const router = useRouter();
+
+  const id = typeof router.query.id === "string" ? router.query.id : "";
+  const { mutate: edit, isLoading } = useEditPost();
+  const { post, isLoading: isPostLoading } = usePost(
+    { postId: id },
+    { enabled: !!id }
+  );
+
+  const initialValues = useMemo(
+    () => ({
+      categories: post?.categories[0] || "",
+      content: post?.content || "",
+      coverImage: {
+        url: post?.coverImage.url || "",
+        value: null,
+      },
+      description: post?.description || "",
+      tags: post?.tags || [],
+      title: post?.title || "",
+    }),
+    [
+      post?.categories,
+      post?.content,
+      post?.coverImage.url,
+      post?.description,
+      post?.tags,
+      post?.title,
+    ]
+  );
+
+  const onSubmit = (values: PostFormFields) => {
+    edit(
+      { ...values, id },
+      {
+        onSuccess: () => router.push("/posts"),
+        onError: (err) =>
+          showNotification({ message: getErrorMessage(err), type: "danger" }),
+      }
+    );
+  };
+
+  const onCancel = () => {
+    router.push("/posts");
+  };
+
+  return (
+    <Box sx={{ position: "relative" }}>
+      <LoadingOverlay visible={isPostLoading} />
+      <PostForm
+        initialValues={initialValues}
+        enableReinitialization
+        onSubmit={onSubmit}
+        isLoading={isLoading}
+        onCancel={onCancel}
+      />
+    </Box>
+  );
 };
 
 PostEdit.getLayout = (page) => {
